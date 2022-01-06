97f3ec14598f9b313a25cb31ef6a9485cc438ccb
Xien Singh <xien@ix.co.za>
2022-01-06 11:29:11 +0200

    Completed the first day course work and first challenge.

---
+++ Day 1/Course Work/main.py
@@ -0,0 +1,22 @@
+# Write your code below this line:
+print("Hello, World!")
+
+# Challenge 1 on https://app.codingrooms.com/w/tT0BsgNxmmwc
+
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+# using multiline string to print the above.
+print("""Day 1 - Python Print Function
+The Function is declared like this:
+print('what to print')""")
+
+
+print("Hello World!\n Hello World!")
+print("Hello World!!\n"*3)
+
+# print the length of the name entered
+print(len(input("What is your name?")))
+
+
