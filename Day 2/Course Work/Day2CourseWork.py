e4763b5a9317975d2b1476885d2cd7f75912f353
Xien Singh <xien@ix.co.za>
2022-01-07 15:59:44 +0200

    To get rid of the errors

--- Day 2/Course Work/Day2CourseWork.py
+++ Day 2/Course Work/Day2CourseWork.py
@@ -1,50 +1,50 @@
  # Data Types
 
  # String
-"Hello World"
+# "Hello World"
 
-#subscripting
-"Hello"[0]
+# subscripting
+# "Hello"[0]
 
-print("Hello"[0]) Output>> "H"
+# print("Hello"[0]) Output>> "H"
 
- #Integer
- 123
- print(1234+1234)
- 123_123_1237
+ # Integer
+ # 123
+ # print(1234+1234)
+ # 123_123_1237
 
  #Float
- 3.145
+ # 3.145
 
  #Boolean
- True
- False
+ # True
+ # False
 
 #Check the type of an object
-type()
+# type()
 
 #Convert int to string
 
-myVar = str(1234)
+# myVar = str(1234)
 
 
 
-PEMDAS (LR):
-Parenthises ()
-Exponents **
-Multiplication *
-Division /
-Addition +
-Subtraction -
+# PEMDAS (LR):
+# Parenthises ()
+# Exponents **
+# Multiplication *
+# Division /
+# Addition +
+# Subtraction -
 
 
 # Rounding numbers
-round(10/3 , 2)
+# round(10/3 , 2)
 
 # Floor Operator - returns int without decimals
-8 // 3
+# 8 // 3
 
 
 # F-Strings
-bob = 1
-print(f"put the values in {bob} to get the values")
+# bob = 1
+# print(f"put the values in {bob} to get the values")
