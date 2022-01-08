3aab7fde3d17e81c55550ec26e03714e51c9cf57
Xien Singh <xiensingh007@gmail.com>
2022-01-08 21:53:58 +0200

    Removed an empty line

--- Day 2/Project/TipCalculator.py
+++ Day 2/Project/TipCalculator.py
@@ -2,7 +2,6 @@ print("Welcome to the tip calculator.")
 billValue = input("What was the total bill? \n")
 percentageTip = input("What percentage tip would you like to give? 10, 12 or 15?\n")
 totalPeople = input("How many people should pay?\n")
-
 totalAfterTip = float(billValue) + (float(billValue) * (float(percentageTip) / 100))
 totalSplit = totalAfterTip / float(totalPeople)
 totalToPay = "{:.2f}".format(totalSplit)
