8e3bc9d78c30b629d8d046eb51a6cc71bd31cccd
Xien Singh <xiensingh007@gmail.com>
2022-01-21 20:58:13 +0200

    Day 16 - Learning how to use Classes and Methods and Functions and Objects in Python

---
+++ Day 16/Course Work/Day16CourseWork.py
@@ -0,0 +1,16 @@
+from turtle import Turtle, Screen
+
+bobo = Turtle()
+my_screen = Screen()
+bobo.shape("turtle")
+bobo.color("Green")
+bobo.forward(100)
+bobo.right(90)
+bobo.forward(100)
+bobo.right(90)
+bobo.forward(100)
+bobo.right(90)
+bobo.forward(100)
+print(my_screen.screensize())
+my_screen.exitonclick()
+
